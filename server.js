e68273367b9bca27cf079e12d809b8ae5470312e
Miha Šušteršič <shooshte@Mihas-MacBook-Pro.local>
2016-12-20 16:34:57 +0100

    changed server to heroku

--- server.js
+++ server.js
@@ -4,14 +4,14 @@ let app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware that redirects to http://
-// app.use(function(req, res, next) {
-//   if(req.headers['x-forwarded-proto'] === 'http') {
-//     next();
-//   } else {
-//     // Redirect to http
-//     res.redirect('http://' + req.hostname + req.url);
-//   }
-// });
+app.use(function(req, res, next) {
+  if(req.headers['x-forwarded-proto'] === 'http') {
+    next();
+  } else {
+    // Redirect to http
+    res.redirect('http://' + req.hostname + req.url);
+  }
+});
 
 app.use(express.static('public'));
 
