30d2efe6ac4ede51adefbdb56b3b47c7f29b25e4
Miha Šušteršič <shooshte@Mihas-MacBook-Pro.local>
2016-12-21 18:58:09 +0100

    added content to the about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,10 @@ let React = require('react');
 let About = (props) => {
   return(
      <div>
-       <h3>About</h3>
-       <p>Welcome to the about page.</p>
+       <h1 className="text-center">About</h1>
+       <p>This simple app is a part of a Udemy course. It's made as a basic demonstration of how to use React.</p>
+       <p>I am using Open Weather Map to search for weather data by city name.</p>
+       <p>There is a github repo. If you're here you already know it.</p>
      </div>
   )
 };
