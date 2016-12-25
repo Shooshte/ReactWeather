6fbd1be90566df81b70659a52ae3a902f6437824
Miha Šušteršič <shooshte@Mihas-MacBook-Pro.local>
2016-12-25 16:08:24 +0100

    added openWeaterAPI icons

--- app/components/WeatherText.jsx
+++ app/components/WeatherText.jsx
@@ -6,10 +6,15 @@ let WeatherText = (props) => {
   let day = props.day;
   let dateObject = new Date(Date.parse(day.dt_txt));
   let readableDate = dateObject.toDateString();
+
+  const iconURL = 'http://openweathermap.org/img/w/';
+  let actualIconURL = `${iconURL}${day.weather[0].icon}.png`
+
   return (
       <div className="waDay">
         <ul>
           <li className="waDate">{readableDate}</li>
+          <li className="waForecastIcon"><img src={actualIconURL} /></li>
           <li>Temperature: {day.main.temp}°C</li>
           <li>Forecast: {day.weather[0].description}</li>
           <li>Wind speed: {day.wind.speed}</li>
