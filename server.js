f6564f4f8564c357ff2461585902b4c761950305
Miha Šušteršič <shooshte@Mihas-MBP.si.sportradar.ag>
2016-12-29 15:52:42 +0100

    moved server to http2, need to change weather data provider

--- server.js
+++ server.js
@@ -1,20 +1,25 @@
 let express = require('express');
 let app = express();
+let spdy = require('spdy');
+let path = require('path');
+let fs = require('fs');
 
 const PORT = process.env.PORT || 3000;
 
-// Middleware that redirects to http://
-app.use(function(req, res, next) {
-  if(req.headers['x-forwarded-proto'] === 'https') {
-    res.redirect('http://' + req.hostname + req.url);
-  } else {
-    // Redirect to http
-    next();
-  }
-});
-
 app.use(express.static('public'));
 
-app.listen(PORT, () => {
-  console.log('Server running on localhost:' + PORT);
-});
+const options = {
+  key: fs.readFileSync(__dirname + '/server.key'),
+  cert: fs.readFileSync(__dirname + '/server.crt')
+}
+
+spdy
+    .createServer(options, app)
+    .listen(PORT, (error) => {
+      if (error) {
+        console.error(error)
+        return process.exit(1)
+      } else {
+        console.log('Listening on port: ' + PORT + '.')
+      }
+    });
