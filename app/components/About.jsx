ddb0920f25aa53b109743617ede0cf78712f7b6e
Miha Šušteršič <shooshte@Mihas-MacBook-Pro.local>
2016-12-29 19:25:16 +0100

    added some forecast styles, updated about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,11 +2,15 @@ let React = require('react');
 
 let About = (props) => {
   return(
-     <div>
-       <h1 className="text-center page-title">About</h1>
-       <p>This simple app is a part of a Udemy course. It's made as a basic demonstration of how to use React.</p>
-       <p>I am using Open Weather Map to search for weather data by city name.</p>
-       <p>There is a github repo. If you're here you already know it.</p>
+     <div className="waCard">
+       <h1 className="waHeader">React Weather App</h1>
+       <h2 className="waSubheader">Created by: <a href="https://github.com/Shooshte" target="_blank">Miha Sustersic</a></h2>
+       <p className="waBodyText">
+          This is a simple weather forecast app, using <a href="https://openweathermap.org/api" target="_black">OpenWeatherMap</a> data.
+       </p>
+       <p className="waBodyText">The app is made using <a href="https://nodejs.org/en/" target="_blank">NodeJS</a> and
+         <a href="https://facebook.github.io/react/" target="_blank"> ReactJS</a>. I tried to follow the <a href="https://material.io/guidelines/" target="_blank">Google material design</a> guidelines. Feel free to point out any flaws, best practices etc.</p>
+       <p className="waBodyText">If interested, click <a href="https://github.com/Shooshte/ReactWeather" target="_blank">here</a> to look at the source code.</p>
      </div>
   )
 };
